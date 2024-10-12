3c16d9daa88e3754ab2eb01a946fcc2a23441680
nboisvert <nicklay@me.com>
2024-10-11 23:47:01 -0400

    Make pink

--- modules/desktop_environment/hyprland/hyprland.nix
+++ modules/desktop_environment/hyprland/hyprland.nix
@@ -17,7 +17,7 @@
     gaps_in = 5;
     gaps_out = 20;
     border_size = 2;
-    "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+    "col.active_border" = "rgba(ff65baee) rgba(ffab7dee) 45deg";
     "col.inactive_border" = "rgba(595959aa)";
     resize_on_border = false; 
     allow_tearing = "false";
