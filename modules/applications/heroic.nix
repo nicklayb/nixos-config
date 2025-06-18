09746d8ab67aaf8aab9988333e729d024fb2dd9f
nicklayb <nicklay@me.com>
2025-06-18 08:54:58 -0400

    Add comment

--- modules/applications/heroic.nix
+++ modules/applications/heroic.nix
@@ -6,6 +6,7 @@
   };
   config = lib.mkIf config.mods.heroic.enable {
     environment.systemPackages = [
+      # Currently having an issue with Electron on stable 24.11
       unstable-pkgs.heroic
     ];
     programs.gamemode.enable = true;
