d7aa68b57d167a7c2f2f358315bf71732af78c32
nicklayb <nicklay@me.com>
2025-10-03 09:56:17 -0400

    Update to hyprland config

--- hosts/garage/configuration.nix
+++ hosts/garage/configuration.nix
@@ -7,11 +7,12 @@
 
   mods._1password.enable = true;
   mods.alacritty.enable = true;
-  mods.bluetooth.enable = true;
   mods.docker.enable = true;
   mods.firefox.enable = true;
   mods.hyprland = {
     enable = true;
+    gaps = 0;
+    dimInactive = false;
     monitor = [
       "eDP-1,1920x1080@60.00Hz,0x0,1"
     ];
@@ -34,8 +35,6 @@
   mods.nautilus.enable = true;
   mods.printing.enable = true;
   mods.inputs.touchscreen.enable = true;
-  mods.waybar.enable = true;
-  mods.waybar.theme = "rose";
   mods.wofi.enable = true;
   mods.zen.enable = true;
 
