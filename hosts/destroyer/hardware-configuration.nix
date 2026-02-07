45935155ac28e84d1a93c5742faf1723c9d051e0
nicklayb <nicklay@me.com>
2026-02-06 19:46:11 -0500

    [25.11] Update script

--- hosts/destroyer/hardware-configuration.nix
+++ hosts/destroyer/hardware-configuration.nix
@@ -1,72 +1,102 @@
-{ config, lib, system, modulesPath, ... }: {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+{
+  config,
+  lib,
+  system,
+  modulesPath,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.loader.systemd-boot.enable = true;
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "ahci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" "v4l2loopback" ];
+  boot.kernelModules = [
+    "kvm-amd"
+    "v4l2loopback"
+  ];
   boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback.out ];
   boot.supportedFilesystems = [ "ntfs" ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1faf9eaf-eba4-4bc5-bbd9-7c1ae8037e89";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/1faf9eaf-eba4-4bc5-bbd9-7c1ae8037e89";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5847-D813";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  fileSystems."/media/ssd" =
-    { device = "/dev/disk/by-uuid/D49650D19650B628";
-      fsType = "ntfs-3g";
-      options = ["rw" "uid=1000"];
-    };
-
-  fileSystems."/media/ssd_pcie" =
-    { device = "/dev/disk/by-uuid/2EB4B14EB4B11973";
-      fsType = "ntfs-3g";
-      options = ["rw" "uid=1000"];
-    };
-
-  fileSystems."/media/ssd_m2" =
-    { device = "/dev/disk/by-uuid/7A4848E94848A5AB";
-      fsType = "ntfs-3g";
-      options = ["rw" "uid=1000"];
-    };
-
-  fileSystems."/media/nouveau_volume" =
-    { device = "/dev/disk/by-uuid/9E380E50380E27BB";
-      fsType = "ntfs-3g";
-      options = ["rw" "uid=1000"];
-    };
-
-  fileSystems."/media/new_volume" =
-    { device = "/dev/disk/by-uuid/609CDD999CDD6A54";
-      fsType = "ntfs-3g";
-      options = ["rw" "uid=1000"];
-    };
-
-  fileSystems."/media/plugins" =
-    { device = "/dev/disk/by-uuid/B65613E35613A363";
-      fsType = "ntfs-3g";
-      options = ["rw" "uid=1000"];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/f092cf58-36b8-4431-8e2e-8a70b34479f4"; }
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/5847-D813";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
     ];
+  };
+
+  fileSystems."/media/ssd" = {
+    device = "/dev/disk/by-uuid/79190843-64c6-40f6-bfc2-fdf8965d4fe5";
+    fsType = "ext4";
+  };
+
+  fileSystems."/media/ssd_pcie" = {
+    device = "/dev/disk/by-uuid/6fced072-23d0-43dd-9e50-49ddf2cc55c7";
+    fsType = "ext4";
+  };
+
+  # fileSystems."/media/ssd_m2" = {
+  #   device = "/dev/disk/by-uuid/7A4848E94848A5AB";
+  #   fsType = "ntfs-3g";
+  #   options = [
+  #     "rw"
+  #     "uid=1000"
+  #   ];
+  # };
+  #
+  # fileSystems."/media/nouveau_volume" = {
+  #   device = "/dev/disk/by-uuid/9E380E50380E27BB";
+  #   fsType = "ntfs-3g";
+  #   options = [
+  #     "rw"
+  #     "uid=1000"
+  #   ];
+  # };
+  #
+  # fileSystems."/media/new_volume" = {
+  #   device = "/dev/disk/by-uuid/609CDD999CDD6A54";
+  #   fsType = "ntfs-3g";
+  #   options = [
+  #     "rw"
+  #     "uid=1000"
+  #   ];
+  # };
+  #
+  # fileSystems."/media/plugins" = {
+  #   device = "/dev/disk/by-uuid/B65613E35613A363";
+  #   fsType = "ntfs-3g";
+  #   options = [
+  #     "rw"
+  #     "uid=1000"
+  #   ];
+  # };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/f092cf58-36b8-4431-8e2e-8a70b34479f4"; }
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault system;
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   hardware.graphics.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
