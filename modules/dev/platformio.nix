d3e7c5b212d0e1e944d6541c450cb8d51145d08b
nicklayb <nicklay@me.com>
2026-09-10 20:36:25 -0400

    [26.05] Add adafruit pro micro

--- modules/dev/platformio.nix
+++ modules/dev/platformio.nix
@@ -4,6 +4,12 @@
   pkgs,
   ...
 }:
+let
+  adafruit_pro_micro = {
+    vendorId = "239a";
+    producId = "8029";
+  };
+in
 {
   options = {
     mods.platformio = {
@@ -15,6 +21,11 @@
     services.udev.packages = [
       pkgs.platformio
       pkgs.platformio-core.udev
+      pkgs.openocd
     ];
+
+    services.udev.extraRules = ''
+      SUBSYSTEM=="tty", ATTRS{idVendor}=="${adafruit_pro_micro.vendorId}", ATTRS{idProduct}=="${adafruit_pro_micro.producId}", MODE="0666"
+    '';
   };
 }
