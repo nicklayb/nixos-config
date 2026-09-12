04a89f25efe9b8c6c550daa909b16ca1adda3367
nicklayb <nicklay@me.com>
2026-09-12 09:39:01 -0400

    [26.05] Bump

--- hosts/hades/hardware-configuration.nix
+++ hosts/hades/hardware-configuration.nix
@@ -29,10 +29,9 @@
     "kvm-intel"
   ];
   boot.supportedFilesystems = [ "ntfs" ];
-  # boot.kernelParams = [
-  #   "mem_sleep_default=s2idle"
-  # ];
-
+  boot.binfmt.emulatedSystems = [
+    # "aarch64-linux"
+  ];
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/d849a473-2322-4544-af66-77f27755974b";
     fsType = "ext4";
